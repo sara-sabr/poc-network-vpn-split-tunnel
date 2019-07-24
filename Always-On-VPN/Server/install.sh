2c8416923bc11dbefa2a5bdbedc37725eda77449
unknown <ERIC.WU@HRSDC-RHDCC.GC.CA>
2019-07-24 15:16:33 -0400

    Push in script and update IPs

--- Always-On-VPN/Server/install.sh
+++ Always-On-VPN/Server/install.sh
@@ -6,6 +6,10 @@ sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
+sudo ufw allow ssh
+sudo ufw allow dns
+sudo ufw enable 
+
 # Install Git to do the pull down since we have no docker repo
 sudo apt install git-core -y
 
@@ -15,4 +19,14 @@ cd ~/poc-setup
 
 # Checkout the code
 git clone https://github.com/sara-sabr/poc-network-vpn-split-tunnel-Azure.git
+
+# Build the DNS server
+cd poc-network-vpn-split-tunnel-Azure/
+pushd .
+
+cd Always-On-VPN/Server/DNS
+sudo docker build . -t esdc-rp-poc-vpn-dns:latest
+sudo docker run  -p 53:53/udp  esdc-rp-poc-vpn-dns:latest
+
+
 
