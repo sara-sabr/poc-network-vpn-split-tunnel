ef835a22232f7f15bd3fc1e7258665a60a4d5415
Eric Wu <e-wu@users.noreply.github.com>
2019-07-29 10:49:54 -0400

    Add git ignore

--- Always-On-VPN/Server/install.sh
+++ Always-On-VPN/Server/install.sh
@@ -3,6 +3,7 @@
 # Install docker on the VM assuming Ubuntu.
 sudo apt-get update
 sudo apt install docker.io -y
+sudo apt install docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
