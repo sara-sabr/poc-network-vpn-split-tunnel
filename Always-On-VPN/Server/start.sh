b3dc2f3de40110c88a0bd51c668c681922d632cc
Eric Wu <e-wu@users.noreply.github.com>
2019-07-29 11:39:11 -0400

    Force start to do a rebuild

--- Always-On-VPN/Server/start.sh
+++ Always-On-VPN/Server/start.sh
@@ -2,5 +2,6 @@
 
 # Get the server's IP address, assuming eth0 is the accessible IP. 
 export DOCKER_PUBLIC_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+docker-compose build
 docker-compose up
 
