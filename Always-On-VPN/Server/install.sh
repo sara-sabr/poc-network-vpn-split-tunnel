a276e5d4b6c1cb3303af71faa69717d6c37a26d9
Eric Wu <e-wu@users.noreply.github.com>
2019-07-29 23:06:37 -0400

    Feature/docker compose server (#22)
    VPN docker image done.

--- Always-On-VPN/Server/install.sh
+++ Always-On-VPN/Server/install.sh
@@ -3,11 +3,13 @@
 # Install docker on the VM assuming Ubuntu.
 sudo apt-get update
 sudo apt install docker.io -y
+sudo apt install docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
 sudo ufw allow ssh
-sudo ufw allow dns
+sudo ufw allow 53
+sudo ufw allow 500,4500/udp
 sudo ufw enable 
 
 # Install Git to do the pull down since we have no docker repo
@@ -20,20 +22,6 @@ cd ~/poc-setup
 # Checkout the code
 git clone https://github.com/sara-sabr/poc-network-vpn-split-tunnel-Azure.git
 
-# Build the DNS server
-cd poc-network-vpn-split-tunnel-Azure/
-pushd .
-
-# Get the server's IP address, assuming eth0 is the accessible IP. 
-listenIP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-
-# Build the DNS Docker image (Move this to a docker repository)
-cd Always-On-VPN/Server/DNS
-sudo docker build . -t esdc-rp-poc-vpn-dns:latest
-
-# Bring up DNS docker
-dockerPort="$listenIP:53"
-sudo docker run  -d -p $dockerPort:53/udp  esdc-rp-poc-vpn-dns:latest
-
-
-
+sudo chmod 700 start.sh
+sudo chmod 700 stop.sh
+sudo ./start.sh
