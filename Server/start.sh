a2bf65cc97915610b783971ff0b0c2d9a488a0ee
Eric Wu <e-wu@users.noreply.github.com>
2019-07-30 09:50:34 -0400

    Add prompts and start compose as daemon

--- Server/start.sh
+++ Server/start.sh
@@ -2,17 +2,20 @@
 
 if [ -z "$SERVER_DOMAIN" ]; then
     echo "Please set environment variable 'SERVER_DOMAIN'."
-    exit
+    read -p "Specify Domain Name :" SERVER_DOMAIN    
+    export SERVER_DOMAIN
 fi
 
 if [ -z "$VPN_IP_CDR" ]; then
     echo "Please set environment variable 'VPN_IP_CDR'. Also, please make sure the mask is greater than /24."
-    exit
+    read -p "Specify VPN_IP_CDR :" VPN_IP_CDR
+    export VPN_IP_CDR
 fi
 
 
 # Get the server's IP address, assuming eth0 is the accessible IP. 
 export DOCKER_PUBLIC_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 docker-compose build
-docker-compose up
+docker-compose up -d
+
 
