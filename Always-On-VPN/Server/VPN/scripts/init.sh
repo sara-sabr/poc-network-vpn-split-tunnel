14449b7e936b2ee4228173b0588a895176f631fb
Eric Wu <e-wu@users.noreply.github.com>
2019-07-29 22:42:08 -0400

    WIP: Add some FW.

--- Always-On-VPN/Server/VPN/scripts/init.sh
+++ Always-On-VPN/Server/VPN/scripts/init.sh
@@ -130,6 +130,8 @@ echo "========================================================================"
 iptables -t nat -A POSTROUTING -s ${VPN_IP_CDR} -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
 iptables -t nat -A POSTROUTING -s ${VPN_IP_CDR} -o eth0 -j MASQUERADE
 iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in  -s ${VPN_IP_CDR} -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+iptables -t filter -A FORWARD --match policy --pol ipsec --dir in --proto esp -s ${VPN_IP_CDR} -j ACCEPT
+iptables -t filter -A FORWARD --match policy --pol ipsec --dir out --proto esp -d ${VPN_IP_CDR} -j ACCEPT
 
 echo "========================================================================"
 echo "VPN Server Starting ..."
